3ae78fa1ad53fb51d35399c4b54b60a599407ba2
Tyler Maxwell <tcmaxwel@ncsu.edu>
2019-10-28 11:43:58 -0400

    ansdj

--- GravityGame/Guis/PrebuiltGuis/PlanetInfoGui.cs
+++ GravityGame/Guis/PrebuiltGuis/PlanetInfoGui.cs
@@ -8,6 +8,7 @@ namespace GravityGame.Guis.PrebuiltGuis
 		private Planet planet;
 
 		private GuiText planet_name;
+		private GuiText composition_text;
 
 		public PlanetInfoGui(Planet planet)
 		{
@@ -22,15 +23,31 @@ namespace GravityGame.Guis.PrebuiltGuis
 			planet_name = new GuiText();
 			planet_name.Contents = "Planet Name";
 			planet_name.Font = Program.Font;
-			planet_name.FontSize = 0.5f;
+			planet_name.FontSize = 1f;
 			planet_name.Color = Color.White;
 
 			column.AddEntry(planet_name);
 			
-			Bar bar = new Bar(120, 3);
-			bar.Margin = new Margin(5, 0, 10, 0);
+			Bar bar = new Bar(200, 3);
+			bar.Margin = new Margin(20, 0, 0, 0);
 			
 			column.AddEntry(bar);
+
+			composition_text = new GuiText();
+			composition_text.Contents = "Composition";
+			composition_text.Font = Program.Font;
+			composition_text.FontSize = 0.5f;
+			composition_text.Color = Color.White;
+
+			column.AddEntry(composition_text);
+
+			Bar bar2 = new Bar(composition_text.Size.X, 2);
+			bar.Margin = new Margin(0, 0, 0, 0);
+			bar2.Color = new Color(200, 200, 200, 255);
+
+			column.AddEntry(bar2);
+			
+			
 		}
 
 		public Gui GetGUI()
